36a561b18ca88307213a71eb97d2568e898829cd
Laura Simoni <laura.simoni@gmail.com>
2021-02-27 15:32:36 +0000

    overlay animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+const btnHamburger = document.getElementById('btnHamburger');
+const header = document.getElementById('header');
+const overlay = document.getElementById('overlay');
+
+btnHamburger.addEventListener('click', ()=>{
+    console.log(header.classList);
+    header.classList.toggle('open');
+    overlay.classList.toggle('fade-in');
+    overlay.classList.toggle('fade-out');
+})
+
